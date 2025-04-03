f11825c88e110315fc20d3fc15aa8a99061761be
Vignesh D <vignesh.d9123@gmail.com>
2025-04-03 15:44:27 +0530

    Update changes

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ dotenv.config()
 const app = express()
 app.use(cors({origin: '*'}))
 app.use(express.json({
-    limit: '50mb'
+    limit: '100mb'
 }))
 app.get('/', (req, res) => {
     res.send('Hello World!')
